aab6103dbe1c4355d118620865daa7e0521fc0eb
chae0 <limchae098@gmail.com>
2023-06-14 13:04:36 +0900

    learn Query Methods

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -3,9 +3,17 @@ package com.sparta.memo.repository;
 import com.sparta.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+    List<Memo> findAllByOrderByModifiedAtDesc();
+    // ModifiedAt 필드 데이터 기준으로 내림차순 하여 전체 데이터를 가져온다.
+    // 정해진 규칙에 맞게 메서드를 선언하면 SimpleJpaRepository 구현체가 생성될 때 자동으로 만들어진다.
 
-
+/*
+    List<Memo> findAllByUsername(String username);
+    // 해당 메서드를 호출할 때 파라미터에 username을 넣어주면 where 조건문에 전달되어 메서드 실행
+*/
 
 }
